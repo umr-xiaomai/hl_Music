ab750c7d70c90975bae5b1cc1d793c7f450cb31e
xiong <xiongyx48@gmail.com>
2026-05-01 19:42:37 +0800

    更新图标

--- KugouAvaloniaPlayer/Program.cs
+++ KugouAvaloniaPlayer/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Runtime.Versioning;
 using System.Threading;
 using Avalonia;
 using Velopack;
-using Velopack.Locators;
-using Velopack.Windows;
 
 namespace KugouAvaloniaPlayer;
 
@@ -19,32 +16,12 @@ internal sealed class Program
     public static void Main(string[] args)
     {
         var velopack = VelopackApp.Build();
-        if (OperatingSystem.IsWindows())
-        {
-#pragma warning disable CA1416
-            velopack
-                .OnAfterInstallFastCallback(_ => RefreshWindowsShortcuts())
-                .OnAfterUpdateFastCallback(_ => RefreshWindowsShortcuts());
-#pragma warning restore CA1416
-        }
 
         velopack.Run();
         _mutex = new Mutex(true, "KugouAvaloniaPlayer", out var createdNew);
         if (!createdNew) return;
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
-    }
-
-    [SupportedOSPlatform("windows")]
-    private static void RefreshWindowsShortcuts()
-    {
-        if (!VelopackLocator.IsCurrentSet) return;
-
-#pragma warning disable CS0618
-        var shortcuts = new Shortcuts(VelopackLocator.Current);
-        var locations = ShortcutLocation.Desktop | ShortcutLocation.StartMenu;
-        shortcuts.CreateShortcutForThisExe(locations);
-#pragma warning restore CS0618
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
